948a885caa349ebbf58a8837d123eecc57d67e03
acrylic-style <me@acrylicstyle.xyz>
2023-10-31 21:29:33 +0900

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,116 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+
+
+import type {
+  VectorizeIndex,
+  Fetcher,
+  Request,
+} from "@cloudflare/workers-types";
+
+import { CloudflareVectorizeStore } from "langchain/vectorstores/cloudflare_vectorize";
+import { CloudflareWorkersAIEmbeddings } from "langchain/embeddings/cloudflare_workersai";
+import { CheerioWebBaseLoader } from 'langchain/document_loaders/web/cheerio'
+import { CloudflareWorkersAI } from 'langchain/llms/cloudflare_workersai'
+import { OpenAI } from 'langchain/llms/openai'
+import {
+  loadQAStuffChain,
+  loadQAMapReduceChain,
+  loadQARefineChain,
+} from "langchain/chains";
+import { Ai } from '@cloudflare/ai';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+  OPENAI_API_KEY: string;
+  VECTORIZE_INDEX: VectorizeIndex;
+  AI: Fetcher;
 }
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
+  async fetch(request: Request, env: Env) {
+    const { pathname, searchParams } = new URL(request.url);
+    const embeddings = new CloudflareWorkersAIEmbeddings({
+      binding: env.AI,
+      modelName: "@cf/baai/bge-small-en-v1.5",
+    });
+    const store = new CloudflareVectorizeStore(embeddings, {
+      index: env.VECTORIZE_INDEX,
+    });
+    if (pathname === "/") {
+      const query = searchParams.get('query')!
+      const results = await store.similaritySearch(query, 5);
+      return Response.json(results);
+    } else if (pathname === '/ask') {
+      try {
+      const query = searchParams.get('query')!
+      const results = (await store.similaritySearchWithScore(query, 20)).sort(d => d[1]).map(d => d[0].pageContent)
+      console.log(results)
+      //const chain = loadQAStuffChain(new CloudflareWorkersAI({ model: '@cf/baai/bge-small-en-v1.5' }))
+      const chain = loadQAStuffChain(new OpenAI({ openAIApiKey: env.OPENAI_API_KEY }))
+      return Response.json(await chain.call({
+        input_documents: results,
+        question: query,
+      }))
+      } catch (e: any) {
+        console.error(e.stack || e)
+      }
+    } else if (pathname === '/insert' && request.method === 'POST') {
+      const body: any = await request.json()
+      const textSplitter = new RecursiveCharacterTextSplitter()
+      const splitted = await textSplitter.transformDocuments(body)
+      splitted.forEach(d => {
+        // d.metadata = {}
+        d.metadata.loc = JSON.stringify(d.metadata.loc)
+      })
+      console.log(`Inserting ${splitted.length} entries`)
+      let i = 0
+      // for (const doc of splitted) {
+      //   console.log(`Processing ${++i} of ${splitted.length}`)
+      //   console.log(JSON.stringify(doc, undefined, 2))
+      //   await store.addDocuments([doc])
+      // }
+      await store.addDocuments(splitted)
+      return Response.json({ success: true })
+    } else if (pathname === "/load") {
+      const article = searchParams.get('article') || 'Cloudflare'
+      // Upsertion by id is supported
+      try {
+        const url = searchParams.get('url') || ('https://en.wikipedia.org/wiki/' + encodeURI(article))
+        console.log('Loading ' + url)
+        const loader = new CheerioWebBaseLoader(url)
+        const docs = await loader.loadAndSplit()
+        docs.forEach(d => {
+          d.metadata.loc = JSON.stringify(d.metadata.loc)
+        })
+        console.log('Adding ' + docs.length + ' entries')
+        await store.addDocuments(docs)
+      } catch (e: any) {
+        console.error(e.stack || e)
+        throw e
+      }
+      // await store.addDocuments(
+      //   [
+      //     {
+      //       pageContent: "hello",
+      //       metadata: {},
+      //     },
+      //     {
+      //       pageContent: "world",
+      //       metadata: {},
+      //     },
+      //     {
+      //       pageContent: "hi",
+      //       metadata: {},
+      //     },
+      //   ],
+      //   { ids: ["id1", "id2", "id3"] }
+      // );
+
+      return Response.json({ success: true });
+    } else if (pathname === "/clear") {
+      await store.delete({ ids: ["id1", "id2", "id3"] });
+      return Response.json({ success: true });
+    }
+
+    return Response.json({ error: "Not Found" }, { status: 404 });
+  },
 };
